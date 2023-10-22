52d827ac75dc0369803c4f4ca74b0d83bb3e5d93
dasha <amayashimizu89@gmail.com>
2023-10-22 18:00:54 +0400

    ВСЕ РАБОТАЕТ И ГИТИГНОР КРАСИВЫЙ

---
+++ WinForms/PluginsConvention.cs
@@ -0,0 +1,211 @@
+﻿using EmployeeTracking.DTOs;
+using EmployeeTracking.Storages;
+using System.Reflection;
+using VisualComponents;
+using WinForms;
+using WinFormsLibrary.OfficePackage.HelperModels;
+using WinFormsLibrary;
+using NonVisualComponents;
+using CustomNonVisualComponent;
+using DocumentFormat.OpenXml.VariantTypes;
+using System.Windows.Forms;
+using WinFormsApp;
+
+namespace PluginsConventionLibraryNet60
+{
+    public class PluginsConvention : IPluginsConvention
+    {
+        CustomListBox customListBox;
+        EmployeeStorage employeeStorage;
+        PositionStorage positionStorage;
+
+        public PluginsConvention()
+        {
+            customListBox = new();
+            employeeStorage = new();
+            positionStorage = new();
+            customListBox.FillTemplateProperties("Идентификатор @Id@, ФИО сотрудника @FullName@, должности @PositionsStr@, подразделение @Department@, стаж работы @YearsOfWork@", '@', '@');
+        }
+
+        public string PluginName
+        {
+            get { return "Стремный список"; }
+        }
+
+        public UserControl GetControl
+        {
+            get
+            {
+                return customListBox;
+            }
+        }
+
+        public PluginsConventionElement GetElement
+        {
+            get
+            {
+                EmployeeListBoxViewModel obj = customListBox.GetObjectFromStr<EmployeeListBoxViewModel>();
+                int? id = Convert.ToInt32(obj.Id);
+
+                byte[] bytes = new byte[16];
+                if (!id.HasValue)
+                {
+                    id = -1;
+                }
+                BitConverter.GetBytes(id.Value).CopyTo(bytes, 0);
+                return new()
+                {
+                    Id = new Guid(bytes)
+                };
+            }
+        }
+
+        public bool CreateChartDocument(PluginsConventionSaveDocument saveDocument)
+        {
+            try
+            {
+                var employees = employeeStorage.GetFullList();
+
+                var experienceData = employees
+                    .GroupBy(e => e.Department)
+                    .ToDictionary(
+                        group => group.Key,
+                        group => new List<double>
+                        {
+                            group.Count(e => e.YearsOfWork >= 1 && e.YearsOfWork <= 5),
+                            group.Count(e => e.YearsOfWork > 5 && e.YearsOfWork <= 10),
+                            group.Count(e => e.YearsOfWork > 10 && e.YearsOfWork <= 20),
+                            group.Count(e => e.YearsOfWork > 20 && e.YearsOfWork <= 30)
+                        }
+                    );
+
+                new DiagramComponent().AddChartToPdf(new()
+                {
+                    Filepath = saveDocument.FileName,
+                    Header = "Группировка сотрудников по подразделениям и диапазонам стажа",
+                    DiagramHeader = "Линейная диаграмма",
+                    LegendPosition = LegendPosition.Right,
+                    XAxisHeaders = new List<string> { "1-5", "5-10", "10-20", "20-30" },
+                    Series = experienceData,
+                });
+            } 
+            catch(Exception)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public bool CreateSimpleDocument(PluginsConventionElement element, PluginsConventionSaveDocument saveDocument)
+        {
+            if (element.Id.GetHashCode() < 0)
+            {
+                return false;
+            }
+
+            var id = element.Id.GetHashCode();
+            var empl = employeeStorage.GetElement(id);
+
+            if (empl == null)
+                return false;
+
+            string filePath = saveDocument.FileName;
+
+            List<string[,]> list1 = new();
+            string[,] arr1 = new string[1, empl.Positions.Count];
+
+            for (int i = 0; i < empl.Positions.Count; i++)
+            {
+                arr1[0, i] = empl.Positions[i];
+            }
+
+            list1.Add(arr1);
+
+            new ComponentTable().CreateTableReport(new ExcelInfo()
+            {
+                FileName = filePath,
+                Title = empl.FullName,
+                Tables = list1
+            });
+            return true;
+        }
+
+        public bool CreateTableDocument(PluginsConventionSaveDocument saveDocument)
+        {
+            try
+            {
+                var employees = employeeStorage.GetFullList();
+                TableInfo<EmployeeViewModel> list = new TableInfo<EmployeeViewModel>()
+                {
+                    FileName = saveDocument.FileName,
+                    Header = "Сотрудники",
+                    Data = employees,
+                    Headers = new List<(int row, string header, string property, int height)>()
+                        {
+                            (0, "ФИО", "FullName", 100),
+                            (1, "Должности", "PositionsStr", 200),
+                            (2, "Подразделение", "Department", 100),
+                            (3, "Стаж работы", "YearsOfWork", 100),
+                        },
+                    MergedCells = new List<(int startInd, int EndInd, string header)>()
+                        {
+                            (2,3,"Работа"),
+                        }
+                };
+                new CustomDocumentTableWord().CreateTable(list);
+            }
+            catch(Exception)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public bool DeleteElement(PluginsConventionElement element)
+        {
+            var result = employeeStorage.Delete(new() { Id = element.Id.GetHashCode() });
+            return result != null;
+        }
+
+        public System.Windows.Forms.Form GetForm(PluginsConventionElement element)
+        {
+            if (element == null)
+            {
+                return new FormEmployee(employeeStorage, positionStorage);
+            }
+            if (element.Id.GetHashCode() > 0)
+            {
+                var form = new FormEmployee(employeeStorage, positionStorage);
+                form.Id = element.Id.GetHashCode();
+                return form;
+            }
+            return null!;
+        }
+
+        public System.Windows.Forms.Form GetThesaurus()
+        {
+            return new FormPositions(positionStorage);
+        }
+
+        public void ReloadData()
+        {
+            var list = employeeStorage.GetFullList();
+            customListBox.ClearCustomListBox();
+            try
+            {
+
+                for (int i = 0; i < list.Count; i++)
+                {
+                    foreach (PropertyInfo propertyInfo in list[i].GetType().GetProperties())
+                    {
+                        customListBox.AddToList(list[i], i, propertyInfo.Name);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
---
+++ WinFormsAppByPluginsNet60/FormMain.cs
@@ -0,0 +1,238 @@
+﻿using PluginsConventionLibraryNet60;
+using System.Reflection;
+
+namespace WinFormsAppByPluginsNet60
+{
+    public partial class FormMain : Form
+    {
+        private readonly Dictionary<string, IPluginsConvention> _plugins;
+        private string _selectedPlugin;
+        public FormMain()
+        {
+            InitializeComponent();
+            _plugins = LoadPlugins();
+            _selectedPlugin = string.Empty;
+            LoadDropDownItems();
+        }
+        private Dictionary<string, IPluginsConvention> LoadPlugins()
+        {
+            Dictionary<string, IPluginsConvention> _plugins = new();
+            string currentDir = Environment.CurrentDirectory;
+            string pluginsDir = Directory.GetParent(currentDir).Parent.Parent.Parent.FullName + "\\Plugins";
+            string[] dllFiles = Directory.GetFiles(pluginsDir, "*.dll", SearchOption.AllDirectories);
+
+            foreach (string dllFile in dllFiles)
+            {
+                try
+                {
+                    Assembly assembly = Assembly.LoadFrom(dllFile);
+                    Type[] types = assembly.GetTypes();
+
+                    foreach (Type type in types)
+                    {
+                        if (typeof(IPluginsConvention).IsAssignableFrom(type) && !type.IsInterface)
+                        {
+                            if (Activator.CreateInstance(type) is IPluginsConvention plugin)
+                            {
+                                string pluginName = plugin.PluginName;
+                                _plugins.Add(pluginName, plugin);
+                            }                            
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Ошибка при загрузке сборки {dllFile}: {ex.Message}");
+                }
+            }
+            return _plugins;
+        }
+        private void LoadDropDownItems()
+        {
+            foreach (var plugin in _plugins)
+            {
+                ToolStripMenuItem pluginMenuItem = new ToolStripMenuItem(plugin.Key);
+                pluginMenuItem.Click += (object? sender, EventArgs a) =>
+                {
+                    _selectedPlugin = plugin.Key;
+                    var userControl = _plugins[plugin.Key].GetControl;
+                    if (userControl != null)
+                    {
+                        panelControl.Controls.Clear();
+                        userControl.Dock = DockStyle.Fill;
+                        _plugins[plugin.Key].ReloadData();
+                        panelControl.Controls.Add(userControl);
+                    }
+                };
+                ControlsStripMenuItem.DropDownItems.Add(pluginMenuItem); 
+            }
+        }
+        private void FormMain_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (string.IsNullOrEmpty(_selectedPlugin) || !_plugins.ContainsKey(_selectedPlugin))
+            {
+                return;
+            }
+            if (!e.Control)
+            {
+                return;
+            }
+            switch (e.KeyCode)
+            {
+                case Keys.I:
+                    ShowThesaurus();
+                    break;
+                case Keys.A:
+                    AddNewElement();
+                    break;
+                case Keys.U:
+                    UpdateElement();
+                    break;
+                case Keys.D:
+                    DeleteElement();
+                    break;
+                case Keys.S:
+                    CreateSimpleDoc();
+                    break;
+                case Keys.T:
+                    CreateTableDoc();
+                    break;
+                case Keys.C:
+                    CreateChartDoc();
+                    break;
+            }
+        }
+        private void ShowThesaurus()
+        {
+            _plugins[_selectedPlugin].GetThesaurus()?.Show();
+        }
+        private void AddNewElement()
+        {
+            var form = _plugins[_selectedPlugin].GetForm(null);
+            if (form != null && form.ShowDialog() == DialogResult.OK)
+            {
+                _plugins[_selectedPlugin].ReloadData();
+            }
+        }
+        private void UpdateElement()
+        {
+            var element = _plugins[_selectedPlugin].GetElement;
+            if (element == null)
+            {
+                MessageBox.Show("Нет выбранного элемента", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            var form = _plugins[_selectedPlugin].GetForm(element);
+            if (form != null && form.ShowDialog() == DialogResult.OK)
+            {
+                _plugins[_selectedPlugin].ReloadData();
+            }
+        }
+        private void DeleteElement()
+        {
+            if (MessageBox.Show("Удалить выбранный элемент", "Удаление", MessageBoxButtons.YesNo, MessageBoxIcon.Question) != DialogResult.Yes)
+            {
+                return;
+            }
+            var element = _plugins[_selectedPlugin].GetElement;
+            if (element == null)
+            {
+                MessageBox.Show("Нет выбранного элемента", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            if (_plugins[_selectedPlugin].DeleteElement(element))
+            {
+                _plugins[_selectedPlugin].ReloadData();
+            }
+        }
+        private void CreateSimpleDoc()
+        {
+            IPluginsConvention plugin;
+            if (!_plugins.TryGetValue(_selectedPlugin, out plugin))
+            {
+                MessageBox.Show("Ауу, загрузи плагин", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            using (SaveFileDialog saveFileDialog = new SaveFileDialog())
+            {
+                saveFileDialog.Filter = "Excel Files (*.xlsx)|*.xlsx|All Files (*.*)|*.*";
+                saveFileDialog.FilterIndex = 1;
+                saveFileDialog.RestoreDirectory = true;
+
+                if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    if (plugin.CreateSimpleDocument(plugin.GetElement, 
+                        new PluginsConventionSaveDocument() { FileName = saveFileDialog.FileName }))
+                    {
+                        MessageBox.Show("Документ сохранен", "Создание документа", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                    else
+                    {
+                        MessageBox.Show("Ошибка при создании документа", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
+        }
+        private void CreateTableDoc()
+        {
+            IPluginsConvention plugin;
+            if (!_plugins.TryGetValue(_selectedPlugin, out plugin))
+            {
+                MessageBox.Show("Ауу, загрузи плагин", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            using (SaveFileDialog saveFileDialog = new SaveFileDialog())
+            {
+                saveFileDialog.Filter = "Word Files (*.docx)|*.docx|All Files (*.*)|*.*";
+                saveFileDialog.FilterIndex = 1;
+                saveFileDialog.RestoreDirectory = true;
+
+                if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    if (plugin.CreateTableDocument(new PluginsConventionSaveDocument() { FileName = saveFileDialog.FileName }))
+                    {
+                        MessageBox.Show("Документ сохранен", "Создание документа", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                    else
+                    {
+                        MessageBox.Show("Ошибка при создании документа", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
+        }
+        private void CreateChartDoc()
+        {
+            IPluginsConvention plugin;
+            if (!_plugins.TryGetValue(_selectedPlugin, out plugin))
+            {
+                MessageBox.Show("Ауу, загрузи плагин", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            using (SaveFileDialog saveFileDialog = new SaveFileDialog())
+            {
+                saveFileDialog.Filter = "PDF Files (*.pdf)|*.pdf|All Files (*.*)|*.*";
+                saveFileDialog.FilterIndex = 1;
+                saveFileDialog.RestoreDirectory = true;
+
+                if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    if (plugin.CreateChartDocument(new PluginsConventionSaveDocument() { FileName = saveFileDialog.FileName }))
+                    {
+                        MessageBox.Show("Документ сохранен", "Создание документа", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                    else
+                    {
+                        MessageBox.Show("Ошибка при создании документа", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
+        }
+        private void ThesaurusToolStripMenuItem_Click(object sender, EventArgs e) => ShowThesaurus();
+        private void AddElementToolStripMenuItem_Click(object sender, EventArgs e) => AddNewElement();
+        private void UpdElementToolStripMenuItem_Click(object sender,EventArgs e) => UpdateElement();
+        private void DelElementToolStripMenuItem_Click(object sender,EventArgs e) => DeleteElement();
+        private void SimpleDocToolStripMenuItem_Click(object sender,EventArgs e) => CreateSimpleDoc();
+        private void TableDocToolStripMenuItem_Click(object sender, EventArgs e) => CreateTableDoc();
+        private void ChartDocToolStripMenuItem_Click(object sender, EventArgs e) => CreateChartDoc();
+    }
+}
